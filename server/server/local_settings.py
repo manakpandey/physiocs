1faffdc593fb62360936e125d6b6a4630462f69c
Shreya Shrivastava <shreya@byteblanket.com>
2021-04-12 21:51:47 +0530

    superuser, login, logout, updatepwd

---
+++ server/server/local_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+
+#################################################################
+    ##  (CORS) Cross-Origin Resource Sharing Settings ##
+#################################################################
+CORS_ORIGIN_ALLOW_ALL = True
