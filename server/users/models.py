dd0c91b0ef4368ff0752447cf2eabda9642b7340
Shreya Shrivastava <shreya@byteblanket.com>
2021-04-14 16:44:10 +0530

    dbs and routes added

--- server/users/models.py
+++ server/users/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class User(models.Model):
+    username= models.CharField(max_length=150)
+    name= models.CharField(max_length=150)
+    password= models.CharField(max_length=250)
+    phno= models.CharField(max_length=50)
+    email= models.CharField(max_length=50)
+    address= models.CharField(max_length=150)
+    userType=models.IntegerField()
+    physioId= models.ForeignKey('self', max_length=150, null=True, blank=True, on_delete=models.PROTECT, related_name='+')
+    docId= models.ForeignKey('self', max_length=150, null=True, blank=True, on_delete=models.PROTECT, related_name='+')
+
+
+class TestDetails(models.Model):
+    testName= models.CharField(max_length=100)
+    jointName= models.CharField(max_length=100)
+    minAngle= models.IntegerField(null=True)
+    maxAngle= models.IntegerField(null=True)
+    reps= models.IntegerField()
+    timePerRep= models.IntegerField(null=True)
+
+
+class TestHistory(models.Model):
+    patientID= models.ForeignKey(User, on_delete=models.PROTECT)
+    testID= models.ForeignKey(TestDetails, on_delete=models.PROTECT)
+    range= models.DecimalField(decimal_places=2,max_digits=5)
+    timeStamp= models.DateTimeField(datetime.now())
+
+
+class Schedule(models.Model):
+    patientID= models.ForeignKey(User, on_delete=models.PROTECT)
+    testID= models.ForeignKey(TestDetails, on_delete=models.PROTECT)
+    date= models.DateField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
--- server/users/urls.py
+++ server/users/urls.py
@@ -1,7 +1,21 @@
 from django.urls import path, include
 from users.views import APILoginView, APILogoutView, APIPasswordUpdateView
+from . import views
+
 urlpatterns = [
     path('login/', APILoginView.as_view(), name='api_login'),
     path('logout/', APILogoutView.as_view(), name='api_logout'),
     path('update_password/', APIPasswordUpdateView.as_view(), name='api_update_password'),
+    path('recovery', views.recovery, name="recovery"),
+    path('results', views.results, name="results"),
+    path('updateUserDetails', views.updateUserDetails, name="updateUserDetails"),
+    path('assignTest', views.assignTest, name="assignTest"),
+    path('getUserDetails', views.getUserDetails, name='getUserDetails'),
+    path('getTestSchedule', views.getTestSchedule, name="getTestSchedule"),
+    path('getTestHistory', views.getTestHistory, name="getTestHistory"),
+    path('getPrediction', views.getPrediction, name="getPrediction"),
+    path('getTests', views.getTests, name="getTests"),
+    path('saveTest', views.saveTest, name="saveTest"),
+    path('saveUserTest', views.saveUserTest, name='saveUserTest'),
+    
 ]
