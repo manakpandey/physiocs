b6b6a4f9c75af1efa22750d75968430dc9679f30
Aditya Ganti <adi10rox@gmail.com>
2021-05-23 19:38:51 +0530

    rep validation

--- client/src/utils/checkRep.js
+++ client/src/utils/checkRep.js
@@ -1,4 +1,5 @@
 function checkAngleWithinLimits(reqdAngle, obtdAngle) {
+    // Absolute difference between the angles
     if(Math.abs(reqdAngle - obtdAngle) < 15)
         return true;
     else 
@@ -6,5 +7,9 @@ function checkAngleWithinLimits(reqdAngle, obtdAngle) {
 }
 
 export default function checkRep(obtdAngles, testDetails, angleAtPos) {
+    // angleAtPos -> which position (flex or relax)
+    // testDetails.jointName -> which joint we're looking for
+    // obtdAngles[] -> the angle associated with the above joint
+    
     return checkAngleWithinLimits(testDetails[angleAtPos],obtdAngles[testDetails.jointName]);
 }
