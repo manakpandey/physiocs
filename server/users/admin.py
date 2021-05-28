9958fbacea270ea8319de28b96f6a9f6ae49bf68
Shreya Shrivastava <shreya@byteblanket.com>
2021-05-28 16:57:18 +0530

    prediction added

--- server/users/admin.py
+++ server/users/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(User)
+admin.site.register(UserDetails)
 admin.site.register(TestDetails)
 admin.site.register(TestHistory)
 admin.site.register(Schedule)
--- server/users/urls.py
+++ server/users/urls.py
@@ -6,16 +6,19 @@ urlpatterns = [
     path('login/', APILoginView.as_view(), name='api_login'),
     path('logout/', APILogoutView.as_view(), name='api_logout'),
     path('update_password/', APIPasswordUpdateView.as_view(), name='api_update_password'),
-    path('recovery', views.recovery, name="recovery"),
     path('results', views.results, name="results"),
     path('updateUserDetails', views.updateUserDetails, name="updateUserDetails"),
-    path('assignTest', views.assignTest, name="assignTest"),
     path('getUserDetails', views.getUserDetails, name='getUserDetails'),
+    path('getUserType', views.getUserType, name='getUserType'),
     path('getTestSchedule', views.getTestSchedule, name="getTestSchedule"),
     path('getTestHistory', views.getTestHistory, name="getTestHistory"),
     path('getPrediction', views.getPrediction, name="getPrediction"),
     path('getTests', views.getTests, name="getTests"),
     path('saveTest', views.saveTest, name="saveTest"),
+    path('scheduleTest', views.scheduleTest, name="scheduleTest"),
+    path('getUserPerfromedTests', views.getUserPerfromedTests, name="getUserPerfromedTests"),
+
     path('saveUserTest', views.saveUserTest, name='saveUserTest'),
+
     
 ]
