f731ce8ef80de5f55a223bcf4528c1b283f59b31
Aditya Ganti <adi10rox@gmail.com>
2021-05-31 15:30:16 +0530

    linked to db

--- client/src/components/ScheduleTest/index.jsx
+++ client/src/components/ScheduleTest/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -8,6 +8,10 @@ import AssignmentIndOutlinedIcon from "@material-ui/icons/AssignmentIndOutlined"
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import axios from "axios";
+import * as settings from "../../settings";
+import { MenuItem } from "@material-ui/core";
+import Swal from "sweetalert2";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -32,15 +36,80 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function ScheduleTest() {
   const classes = useStyles();
 
-  const [joint, setJoint] = useState("");
+  const [ptntData, setPtntData] = useState([]);
+  const [testData, setTestData] = useState([]);
+  const [patient, setPatient] = useState(0);
+  const [test, setTest] = useState(0);
 
-  const handleJoint = (event) => {
-    setJoint(event.target.value);
-    console.log(joint);
+  async function handleSubmit() {
+    let formData = new FormData();
+    formData.append("pid", patient);
+    formData.append("tid", test);
+
+    for (var pair of formData.entries()) {
+         console.log(pair[0] + ", " + pair[1]);
+       }
+
+    try {
+      const res = await axios.post(
+        `${settings.API_SERVER}/api/auth/scheduleTest`,
+        formData
+      );
+
+      if(res.status == 201) {
+        Swal.fire({
+          icon: "success",
+          title: "Succesful",
+          text: "Test Assigned Successfully",
+        });
+
+        setPatient("");
+        setTest("");
+      }
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const resPtnt = await axios.get(
+        `${settings.API_SERVER}/api/auth/getPhysioUsers`,
+        { withCredentials: true }
+      );
+      console.log(resPtnt.data);
+      setPtntData(resPtnt.data);      
+    } catch (e) {
+      console.log(e);
+    }
+
+    try {
+      const resTest = await axios.get(
+        `${settings.API_SERVER}/api/auth/getAllTests`,
+        { withCredentials: true }
+      );
+      console.log(resTest.data);
+      setTestData(resTest.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
+
+  const handleTest = (event) => {
+    setTest(event.target.value);
+  }
+
+  const handlePatient = (event) => {
+    setPatient(event.target.value);
+  }
 
   return (
     <Container component="main" maxWidth="xs">
@@ -62,8 +131,15 @@ export default function ScheduleTest() {
                 required
                 variant="outlined"
                 select
-                //value={''}
-              ></TextField>
+                onChange={handlePatient}
+                value={patient}
+              >
+                {ptntData.map((ptnt, index) => {
+                  return <MenuItem key={index} value={ptnt.id}>
+                    {ptnt.first_name + ' ' + ptnt.last_name}
+                  </MenuItem>
+                })}
+              </TextField>
             </Grid>
 
             <Grid item xs={12}>
@@ -72,26 +148,27 @@ export default function ScheduleTest() {
                 id="demo-simple-select"
                 fullWidth
                 label="Select Test"
-                value={joint}
-                onChange={handleJoint}
                 required
                 select
                 variant="outlined"
+                onChange={handleTest}
+                value={test}
               >
-                {/*joints.map((joint) => {
-                  return <MenuItem value={joint} key={joint}>
-                  {joint}
-                </MenuItem>
-                })*/}
+                {testData.map((test, index) => {
+                  return <MenuItem value={test.id} key={index}>
+                    {test.test_name}
+                  </MenuItem>
+                })}
               </TextField>
             </Grid>
           </Grid>
           <Button
-            type="submit"
+            type="button"
             fullWidth
             variant="contained"
             color="primary"
             className={classes.submit}
+            onClick={handleSubmit}
           >
             Schedule Test
           </Button>
